68be7fca32d911dd89fb7f4b6dfb987d6a0db2b7
perry <pasibeybonsu@drew.edu>
2020-05-16 10:40:24 -0400

    Comments added to explore logic

--- lib/phone_number.dart
+++ lib/phone_number.dart
@@ -1,32 +1,47 @@
 class PhoneNumber {
   // Put your code here
   String clean(String input) {
+    //This takes the string and gets rid of unwanted characters
     input = strip(input);
 
+    //This escapes the function if there are any letters found in the number
     if (!isNum(input)) return null;
 
-    // Length 11 + area code ! check
+    // If the length of the number is 11 and starts with a 1, it is still valid
+    //just take off the one
     if (input.length==11 && input.startsWith('1')) {
       input = input.substring(1);
     }
-    //Less than 9 check
+    //If the length of the number is less than 9 return null, this is not
+    //a valid input
       if (input.length!=10) {
         return null;
       }
 
-      //Exchange code check
+      //This applies to an input number that has already been processed by the
+    //strip() function and is the appropriate length. This returns null if the
+    //area code starts with a 1 or 0 by checking the first element of the string
+    //and also returns null if the escape code is 1 or 0 by checking the third element
+    //of the string
       if (['0', '1'].contains(input[0]) || ['0', '1'].contains(input[3])) return null;
 
+      //If the input passes all the conditions above return it
       return input;
     }
 
-  //Remove unwanted characters
+//  The strip() function removes unwanted characters
+////  This method takes in a string, and breaks it into an array of characters
+////  that are not unwanted characters(split). Then it takes that array of characters
+////  and combines it into a single, new string (fold).
   String strip(String str) => str
       .split('')
       .where((ch) => ![".", " ", "(", ")", "-", "+"].contains(ch))
       .fold("", (str, elem) => str + elem);
-  //Individual digit checker
+
+  //The isDigit() function checks a given character to see if it is a number
   bool isDigit (String ch) => ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].contains(ch);
-  //Total number checker
+
+  //The isNum() function splits a string into an array and checks each element
+  //with the the isDigit() function. Any non-number found in the array returns false
   bool isNum(String str) => str.split('').every(isDigit);
   }
